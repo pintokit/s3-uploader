cc7ab60b40dfa9759b3669e9de63fde6bab732b8
David Solis <github@soliskit.com>
2020-01-07 15:03:30 -0800

    print request error to console

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ function getProfile(username) {
                 printMessage(username, profile.badges.length, profile.points.JavaScript); 
     });
   });
+
+  request.on('error', error => console.error(`Problem with request: ${error.message}`));
 }
 
 // const users = ["chalkers", "alenaholligan", "davemcfarland"];
