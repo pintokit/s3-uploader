20eee074bca524cf115c0e373d4742c993411448
David Solis <github@soliskit.com>
2020-01-08 15:19:08 -0800

    save uploaded file to memory

--- app.js
+++ app.js
@@ -15,10 +15,28 @@ const s3 = new AWS.S3({
 
 const app = express()
 const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, limits: { fileSize: 20 * 1000 * 1000 } }).single('photoUpload')
 
 app.get('/', function(request, response) {
   response.send('<form method="post" enctype="multipart/form-data" action="/">' + '<input name="photoUpload" type="file" accept="image/*">' + '<input type="submit">' + '</form>')
+})
+
+app.post('/', function(request, response) {
+  if (!request.file) {
+    return response.status(500).send('No file selected')
+  } else {
+    upload(request, response, function(error) {
+      if (error instanceof multer.MulterError) {
+        console.error(error.message)
+        return response.status(500).send(`${error.message}`)
+      } else if (error) {
+        console.error(error.message)
+        return response.status(500).send(`${error.message}`)
+      }
+      console.log('File uploaded')
+      return response.status(200).send('File uploaded')
+    })
+  }
 })
 
 app.listen(5000)
