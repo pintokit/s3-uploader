4f85d91ebfa15ead712c2e660654e7928519010c
David Solis <github@soliskit.com>
2020-01-07 20:55:39 -0800

    upload photo to s3

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// file path is required but file extension is not
-const profile = require(`./profile.js`)
+require('dotenv').config()
+// // file path is required but file extension is not
+// const profile = require(`./profile.js`)
 
-const users = ["chalkers", "alenaholligan", "davemcfarland"];
-// `process` is a global object that allows access to arguments passed in to the command line
-// const users = process.argv.slice(2);
+// const users = ["chalkers", "alenaholligan", "davemcfarland"];
+// // `process` is a global object that allows access to arguments passed in to the command line
+// // const users = process.argv.slice(2);
 
-// users.forEach (username => {
-//     getProfile(username);
-// });
+// // users.forEach (username => {
+// //     getProfile(username);
+// // });
 
-users.forEach(profile.get);
+// users.forEach(profile.get);
 
 const https = require('http');
 
@@ -21,3 +22,40 @@ https.createServer(function(request, response) {
 }).listen(port, function() {
   console.log(port);
 });
+
+const ID = process.env.S3_ACCESS_KEY_ID;
+const SECRET = process.env.S3_SECRET_ACCESS_KEY;
+const BUCKET_NAME = process.env.S3_BUCKET_NAME;
+
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+      Bucket: BUCKET_NAME,
+      Key: fileName,
+      Body: fileContent
+  };
+
+  const promise = s3.upload(params).promise()
+
+  promise.then(
+    function(data) {
+      console.log(`Photo uploaded successfully: ${data.location}`);
+    }, 
+    function(error) {
+      return console.error("Error uploading photo:", errror.message);
+    }
+  );
+};
+
+uploadFile('sample.png');
