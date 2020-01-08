0dd8c84b19fc10041b25c1e99a0b9f647c30593e
David Solis <github@soliskit.com>
2020-01-08 13:10:48 -0800

    encapsulate user profile example

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
 require('dotenv').config()
-// // file path is required but file extension is not
-// const profile = require(`./profile.js`)
-
-// const users = ["chalkers", "alenaholligan", "davemcfarland"];
-// // `process` is a global object that allows access to arguments passed in to the command line
-// // const users = process.argv.slice(2);
-
-// // users.forEach (username => {
-// //     getProfile(username);
-// // });
-
-// users.forEach(profile.get);
 
 const http = require('http');
 
@@ -65,5 +53,3 @@ const uploadFile = (fileName) => {
     }
   );
 };
-
-// uploadFile('sample.png');
