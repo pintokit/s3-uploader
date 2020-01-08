4be4cfb8e2c509a687e07d3ef03f850dea4a27cb
David Solis <github@soliskit.com>
2020-01-07 17:06:19 -0800

    display hello world

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ const users = ["chalkers", "alenaholligan", "davemcfarland"];
 // });
 
 users.forEach(profile.get);
+
+const https = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 5000;
+
+https.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(port, hostname, function() {
+  console.log("Server running at http://" + hostname + ":" + port + "/");
+});
