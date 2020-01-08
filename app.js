961b6b973810d222c1aff2a93e9502f9d63d3dbe
David Solis <github@soliskit.com>
2020-01-07 16:16:23 -0800

    print console error with http status code

--- app.js
+++ app.js
@@ -14,20 +14,26 @@ function printMessage(username, badgeCount, point) {
 function getProfile(username) {
   try {
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      let body = "";
+      if (response.statusCode == 200) {
+        let body = "";
   
-      response.on('data', data => {
-        body += data.toString();
-      });
-      
-      response.on('end', () => {
-        try {
-          const profile = JSON.parse(body);
-          printMessage(username, profile.badges.length, profile.points.JavaScript); 
-        } catch (error) {
-          printError(error);
-        }
-      });
+        response.on('data', data => {
+          body += data.toString();
+        });
+        
+        response.on('end', () => {
+          try {
+            const profile = JSON.parse(body);
+            printMessage(username, profile.badges.length, profile.points.JavaScript); 
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${response.statusCode})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
     });
   
     request.on('error', error => console.error(`Problem with request: ${error.message}`));
