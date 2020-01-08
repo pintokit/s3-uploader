fae643731533ffdd8f690e76cd206b9b779585d6
David Solis <github@soliskit.com>
2020-01-07 16:26:09 -0800

    encapsulate profile code into module

--- app.js
+++ app.js
@@ -1,47 +1,5 @@
-// require https module
-const https = require('https');
-const http = require(`http`);
-
-// Print Error Messages
-function printError(error) {
-  console.error(error.message);
-}
-
-function printMessage(username, badgeCount, point) {
-  const message = `${username} has ${badgeCount} total badge(s) and ${point} in JavaScript`;
-  console.log(message);
-}
-
-function getProfile(username) {
-  try {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      if (response.statusCode == 200) {
-        let body = "";
-  
-        response.on('data', data => {
-          body += data.toString();
-        });
-        
-        response.on('end', () => {
-          try {
-            const profile = JSON.parse(body);
-            printMessage(username, profile.badges.length, profile.points.JavaScript); 
-          } catch (error) {
-            printError(error);
-          }
-        });
-      } else {
-        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-        const statusCodeError = new Error(message);
-        printError(statusCodeError);
-      }
-    });
-  
-    request.on('error', error => console.error(`Problem with request: ${error.message}`));
-  } catch (error) {
-    printError(error);
-  }
-}
+// file path is required but file extension is not
+const profile = require(`./profile.js`)
 
 // const users = ["chalkers", "alenaholligan", "davemcfarland"];
 // `process` is a global object that allows access to arguments passed in to the command line
@@ -51,4 +9,4 @@ const users = process.argv.slice(2);
 //     getProfile(username);
 // });
 
-users.forEach(getProfile);
+users.forEach(profile.get);
