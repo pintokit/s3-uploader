507c267b8a234b473a518076a7b2a2ceb6574e63
David Solis <github@soliskit.com>
2020-01-07 16:57:57 -0800

    replace command line prompt with array literal

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // file path is required but file extension is not
 const profile = require(`./profile.js`)
 
-// const users = ["chalkers", "alenaholligan", "davemcfarland"];
+const users = ["chalkers", "alenaholligan", "davemcfarland"];
 // `process` is a global object that allows access to arguments passed in to the command line
-const users = process.argv.slice(2);
+// const users = process.argv.slice(2);
 
 // users.forEach (username => {
 //     getProfile(username);
