cd92d86df83e350754bf93ac25905062cb18b84e
David Solis <github@soliskit.com>
2020-01-08 19:54:11 -0800

    formatting

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-const fs = require('fs')
 const AWS = require('aws-sdk')
 const express = require('express')
 const multer  = require('multer')
@@ -20,7 +19,7 @@ const app = express()
 const storage = multerS3({
   s3: s3, 
   bucket: BUCKET_NAME,
-  key: function (req, file, setKey) {
+  key: function (_request, file, setKey) {
     fileName = Date.now().toString() + file.mimetype.replace('image/', '.')
     setKey(null, fileName)
   }
@@ -28,11 +27,11 @@ const storage = multerS3({
 
 const upload = multer({storage: storage, limits: { fileSize: 20 * 1000 * 1000 } }).single('photoUpload')
 
-app.get('/', function(request, response) {
+app.get('/', function(_request, response) {
   response.send('<form method="post" enctype="multipart/form-data" action="/">' + '<input name="photoUpload" type="file" accept="image/*">' + '<input type="submit">' + '</form>')
 })
 
-app.get('/image', function (request, response) {
+app.get('/image', function (_request, response) {
   response.send('<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><image href="https://photo-uploader-dev.s3-us-west-1.amazonaws.com/1578540232146.jpeg" height="200" width="200"></image></svg>')
 })
 
@@ -45,7 +44,7 @@ app.post('/', function(request, response) {
       console.error(error.message)
       response.status(500).send(`${error.message}`)
     }
-    const fileUrl = `https://photo-uploader-dev.s3-us-west-1.amazonaws.com/${fileName}`
+    const fileUrl = `https://${BUCKET_NAME}.s3-us-west-1.amazonaws.com/${fileName}`
     response.status(200).json({assetUrl: fileUrl})
   })
 })
