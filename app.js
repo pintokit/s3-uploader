9b287f938cfeee7c4daf6b649fd18cb674bfbd31
David Solis <github@soliskit.com>
2020-01-08 19:39:54 -0800

    return s3 fileUrl as JSON

--- app.js
+++ app.js
@@ -14,15 +14,18 @@ const s3 = new AWS.S3({
   secretAccessKey: SECRET
 })
 
+let fileName
+
 const app = express()
 const storage = multerS3({
   s3: s3, 
   bucket: BUCKET_NAME,
   key: function (req, file, setKey) {
-    let imageType = file.mimetype.replace('image/', '.')
-    setKey(null, Date.now().toString() + imageType)
+    fileName = Date.now().toString() + file.mimetype.replace('image/', '.')
+    setKey(null, fileName)
   }
 })
+
 const upload = multer({storage: storage, limits: { fileSize: 20 * 1000 * 1000 } }).single('photoUpload')
 
 app.get('/', function(request, response) {
@@ -38,8 +41,8 @@ app.post('/', function(request, response) {
       console.error(error.message)
       response.status(500).send(`${error.message}`)
     }
-    console.log('File uploaded')
-    response.status(200).send('File uploaded')
+    const fileUrl = `https://photo-uploader-dev.s3-us-west-1.amazonaws.com/${fileName}`
+    response.status(200).json({assetUrl: fileUrl})
   })
 })
 
